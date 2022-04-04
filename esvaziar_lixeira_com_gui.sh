ca16f34a9f96cfe4baef690cd9cdff15429d40ef
gabriel-cavalcante-de-jesus-oliveira <gabriel.lcifba@gmail.com>
2022-04-04 12:37:20 -0300

    Código comentado

--- esvaziar_lixeira_com_gui.sh
+++ esvaziar_lixeira_com_gui.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# é necessário ter o pacote "dialog" instalado na máquina
+# é necessário ter o pacote "dialog" instalado na máquina (rode "sudo apt install dialog -y")
 # o "dialog" é utilizado para criação de caixas de diálogo do tipo TUI "Text User Interface"
 function esvaziarLixeira {
 	rm -rfv ~/.local/share/Trash/files/* 1> ~/.log_de_arquivos_removidos.txt # arquivo oculto usado para registro
 }
 function sairPrograma {
-	dialog --infobox 'Saíndo do programa...' 0 0 # parâmetros 0 0 se referem a altura e largura da caixa, 0 0 para que o dialog ajuste dinamicamente para a gente
+	# 0 0 são parâmetros para altura e largura, respectivamente, 0 0 para que o dialog ajuste dinamicamente para a gente
+	dialog --infobox 'Saíndo do programa...' 0 0
 	sleep $TEMPO_SONO
 	clear
 	exit
 }
 declare -r TEMPO_SONO=1
-dialog --title 'Esvaziar lixeira' --yesno 'Tem certeza que deseja remover todo o conteúdo da lixeira?' 0 0 # retorna código de retorno para a variável especial "$?"
+#retorna código de retorno para a variável especial "$?"
+dialog --title 'Esvaziar lixeira' --yesno 'Tem certeza que deseja remover todo o conteúdo da lixeira?' 0 0
 if [ $? = 0 ] # 0 para "sim" e 1 para "não"
 then
 	dialog --infobox 'Removendo todo o conteúdo da lixeira...' 0 0
