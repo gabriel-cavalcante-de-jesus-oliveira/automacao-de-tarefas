a2f3660c91546fd2e89adccbb5757d10fd0c8a4e
gabriel-cavalcante-de-jesus-oliveira <gabriel.lcifba@gmail.com>
2022-04-04 12:23:15 -0300

    Resumido caminho de arquivos

--- esvaziar_lixeira_com_gui.sh
+++ esvaziar_lixeira_com_gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function esvaziarLixeira {
-	rm -rfv ~/.local/share/Trash/files/* 1> /home/gabriel/.log_de_arquivos_removidos.txt # arquivo oculto usado para registro
+	rm -rfv ~/.local/share/Trash/files/* 1> ~/.log_de_arquivos_removidos.txt # arquivo oculto usado para registro
 }
 function sairPrograma {
 	dialog --infobox 'Saíndo do programa...' 0 0
@@ -9,14 +9,14 @@ function sairPrograma {
 	exit
 }
 declare -r TEMPO_SONO=1
-dialog --title "Esvaziar lixeira" --yesno "Tem certeza que deseja remover todo o conteúdo da lixeira?" 0 0
+dialog --title 'Esvaziar lixeira' --yesno 'Tem certeza que deseja remover todo o conteúdo da lixeira?' 0 0
 if [ $? = 0 ]
 then
 	dialog --infobox 'Removendo todo o conteúdo da lixeira...' 0 0
 	sleep $TEMPO_SONO
 	esvaziarLixeira
-	dialog --textbox /home/gabriel/.log_de_arquivos_removidos.txt 0 0
-	rm /home/gabriel/.log_de_arquivos_removidos.txt # exclui o arquivo quando não for mais necessário
+	dialog --textbox ~/.log_de_arquivos_removidos.txt 0 0
+	rm ~/.log_de_arquivos_removidos.txt # exclui o arquivo quando não for mais necessário
 	clear
 	dialog --infobox 'Concluído!' 0 0
 	sleep $TEMPO_SONO
