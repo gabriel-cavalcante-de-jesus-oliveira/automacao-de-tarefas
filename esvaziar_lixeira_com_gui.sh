0127cf8770c8d6dbf4e151d3d3666a1305f8d151
gabriel-cavalcante-de-jesus-oliveira <gabriel.lcifba@gmail.com>
2022-04-05 12:26:00 -0300

    Melhorada visualização do log

--- esvaziar_lixeira_com_gui.sh
+++ esvaziar_lixeira_com_gui.sh
@@ -2,7 +2,10 @@
 # é necessário ter o pacote "dialog" instalado na máquina (rode "sudo apt install dialog -y")
 # o "dialog" é utilizado para criação de caixas de diálogo do tipo TUI "Text User Interface"
 function esvaziarLixeira {
-	rm -rfv ~/.local/share/Trash/files/* 1> ~/.log_de_arquivos_removidos.txt # arquivo oculto usado para registro
+	rm -rfv ~/.local/share/Trash/files/*
+}
+function logArquivosRemovidos { # cria log de arquivos excluídos
+	tree ~/.local/share/Trash/files 1> ~/.log_de_arquivos_removidos.txt # arquivo oculto
 }
 function sairPrograma {
 	# 0 0 são parâmetros para altura e largura, respectivamente, 0 0 para que o dialog ajuste dinamicamente para a gente
@@ -18,9 +21,10 @@ if [ $? = 0 ] # 0 para "sim" e 1 para "não"
 then
 	dialog --infobox 'Removendo todo o conteúdo da lixeira...' 0 0
 	sleep $TEMPO_SONO
+	logArquivosRemovidos # deve ser invocada antes da função abaixo
 	esvaziarLixeira
-	dialog --textbox ~/.log_de_arquivos_removidos.txt 0 0
-	rm ~/.log_de_arquivos_removidos.txt # exclui o arquivo quando não for mais necessário
+	dialog --textbox ~/.log_de_arquivos_removidos.txt 0 0 # exibe o log
+	rm -f ~/.log_de_arquivos_removidos.txt # exclui o arquivo de log
 	clear
 	dialog --infobox 'Concluído!' 0 0
 	sleep $TEMPO_SONO
