66dc9450099fb5a6539953317d056ce4c5610354
gabriel-cavalcante-de-jesus-oliveira <gabriel.lcifba@gmail.com>
2022-04-05 12:47:03 -0300

    Código mais eficiente e legível

--- esvaziar_lixeira_com_gui.sh
+++ esvaziar_lixeira_com_gui.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # é necessário ter o pacote "dialog" instalado na máquina (rode "sudo apt install dialog -y")
+# é necessário ter o pacote "tree" instalado na máquina (rode "sudo apt install tree -y")
 # o "dialog" é utilizado para criação de caixas de diálogo do tipo TUI "Text User Interface"
 function esvaziarLixeira {
-	rm -rfv ~/.local/share/Trash/files/*
+	# não é necessário acessar o caminho da lixeira novamente, pois a função abaixo já o faz
+	rm -rf *
 }
 function logArquivosRemovidos { # cria log de arquivos excluídos
-	tree ~/.local/share/Trash/files 1> ~/.log_de_arquivos_removidos.txt # arquivo oculto
+	cd $CAMINHO_LIXEIRA
+	tree . 1> ~/.log_de_arquivos_removidos.txt # arquivo oculto
 }
 function sairPrograma {
 	# 0 0 são parâmetros para altura e largura, respectivamente, 0 0 para que o dialog ajuste dinamicamente para a gente
@@ -14,17 +17,20 @@ function sairPrograma {
 	clear
 	exit
 }
-declare -r TEMPO_SONO=1 # a unidade de tempo é o segundo
+declare -r TEMPO_SONO=1
+declare -r CAMINHO_LIXEIRA="/home/gabriel/.local/share/Trash/files" # tem que ser o caminho absoluto
+declare -r CAMINHO_HOME="/home/gabriel"
 #retorna código de retorno para a variável especial "$?"
 dialog --title 'Esvaziar lixeira' --yesno 'Tem certeza que deseja remover todo o conteúdo da lixeira?' 0 0
 if [ $? = 0 ] # 0 para "sim" e 1 para "não"
 then
 	dialog --infobox 'Removendo todo o conteúdo da lixeira...' 0 0
-	sleep $TEMPO_SONO
+	sleep $TEMPO_SONO # a unidade de tempo é o segundo
 	logArquivosRemovidos # deve ser invocada antes da função abaixo
 	esvaziarLixeira
-	dialog --textbox ~/.log_de_arquivos_removidos.txt 0 0 # exibe o log
-	rm -f ~/.log_de_arquivos_removidos.txt # exclui o arquivo de log
+	cd $CAMINHO_HOME
+	dialog --textbox .log_de_arquivos_removidos.txt 0 0 # exibe o log
+	rm -f .log_de_arquivos_removidos.txt # exclui o arquivo de log
 	clear
 	dialog --infobox 'Concluído!' 0 0
 	sleep $TEMPO_SONO
