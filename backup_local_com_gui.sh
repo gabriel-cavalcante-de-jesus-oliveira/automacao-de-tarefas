6db849437e53cfe2f152f8a435390dc7bc8b1e97
gabriel-cavalcante-de-jesus-oliveira <gabriel.lcifba@gmail.com>
2022-04-07 11:45:32 -0300

    Código refatorado

--- backup_local_com_gui.sh
+++ backup_local_com_gui.sh
@@ -6,36 +6,37 @@ then
     mkdir "/home/gabriel/.backup"
 fi
 function removeBackupAntigo {
-	dialog --infobox 'Removendo backup antigo...' 0 0
+	dialog --infobox "Removendo backup antigo..." 0 0
     sleep $TEMPO_SONO
-    rm -rf "$/home/gabriel/.backup"
+	cd ~/.backup
+    rm -rf *
 }
 function realizaNovoBackup {
-	dialog --infobox 'Realizando novo backup...' 0 0
+	dialog --infobox "Realizando novo backup..." 0 0
 	sleep $TEMPO_SONO
-	cp -rf "/home/gabriel/Área de trabalho/quinto_semestre_de_computacao" "/home/gabriel/.backup"
-    cp -rf "/home/gabriel/Documentos" "/home/gabriel/.backup"
-    cp -rf "/home/gabriel/Downloads" "/home/gabriel/.backup"
-    cp -rf "/home/gabriel/Imagens" "/home/gabriel/.backup"
-    cp -rf "/home/gabriel/Modelos" "/home/gabriel/.backup"
-    cp -rf "/home/gabriel/Música" "/home/gabriel/.backup"
-    cp -rf "/home/gabriel/Público" "/home/gabriel/.backup"
-    cp -rf "/home/gabriel/Vídeos" "/home/gabriel/.backup"
-	cp -rf "/home/gabriel/temp" "/home/gabriel/.backup"
-	tree ~/.backup 1> ~/.log_de_arquivos_copiados.txt
-	dialog --textbox ~/.log_de_arquivos_copiados.txt 0 0
-	rm -f ~/.log_de_arquivos_copiados.txt
+	cd $HOME
+	cp -rf Área\ de\ trabalho/quinto_semestre_de_computacao .backup
+    cp -rf Documentos .backup
+    cp -rf Downloads .backup
+    cp -rf Imagens .backup
+    cp -rf Modelos .backup
+    cp -rf Música .backup
+    cp -rf Público .backup
+    cp -rf Vídeos .backup
+	cp -rf temp .backup
+	tree .backup 1> .log_de_arquivos_copiados.txt
+	dialog --textbox .log_de_arquivos_copiados.txt 0 0
+	rm -f .log_de_arquivos_copiados.txt
     sleep $TEMPO_SONO
 }
 function sairPrograma {
-	dialog --infobox 'Saíndo do programa' 0 0
+	dialog --infobox "Saíndo do programa" 0 0
 	sleep $TEMPO_SONO
 	clear
 	exit
 }
 removeBackupAntigo
 realizaNovoBackup
-dialog --infobox 'Concluído!' 0 0
+dialog --infobox "Concluído!" 0 0
 sleep $TEMPO_SONO
 sairPrograma
-exit
